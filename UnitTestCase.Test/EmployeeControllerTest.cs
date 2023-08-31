1851640e61c9a645fff4688f620361e26db5ac57
gaurav123bhekare <gaurav.bhekare7@gmail.com>
2023-08-31 22:36:19 +0530

    Add files via upload

--- UnitTestCase.Test/EmployeeControllerTest.cs
+++ UnitTestCase.Test/EmployeeControllerTest.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Net;
 using WebApp_CRUD_Core.Controllers;
 using WebApp_CRUD_Core.Model;
 using WebApp_CRUD_Core.Repository;
@@ -11,58 +14,39 @@ namespace UnitTestCase.Test
     {
         EmployeeController _controller;
         IEmployeeRepository _repository = null;
+        public readonly Mock<IEmployeeRepository> engagementsService = new Mock<IEmployeeRepository>();
 
-     
 
         public EmployeeControllerTest()
         {
             if (_repository ==null)
             {
-                _controller = new EmployeeController(_repository);
-            }
-            
+                _controller = new EmployeeController(engagementsService.Object);
+                _controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            }            
         }
         [Fact]
         public void GetEmployee_Sucess()
-        {
-            // Arrage 
-
-            // Act 
+        {  // Act 
             var result = _controller.Get();
-            //var resultType = result as OkObjectResult;
-            //var resultList = resultType.Value as List<Employee>;
-
+           
             // Assert
-            Assert.NotNull(result);
-            //Assert.IsType<List<Employee>>(resultType.Value);
-            //Assert.Equal(3, resultList.Count);
+            Assert.NotNull(result);            
         }
 
         [Fact]
-        public async Task AddEmployee_Success()
+        public void AddEmployee_Success()
         {
             Employee emp = new Employee()
             {
-                EmployeeID = 5,
-                FirstName = "Sanjay",
-                LastName = "Singh",
-                EmailId = "Sanjay.Singh02@gmail.com",
-                Age = 21
+                EmployeeID = 0,
+                FirstName = "Sanjay1",
+                LastName = "Singh1",
+                EmailId = "Sanjay1.1Singh02@gmail.com",
+                Age = 23
             };
-
-            var response =await _controller.Post(emp);
-            Assert.IsType<CreatedAtActionResult>(response);
-
-            var createdEmp = response as CreatedAtActionResult;
-            Assert.IsType<Employee>(createdEmp.Value);
-            var employeeItem = createdEmp.Value as Employee;
-
-            Assert.Equal("Sanjay", emp.FirstName);
-            Assert.Equal("Singh", emp.LastName);
-            Assert.Equal("Sanjay.Singh02@gmail.com", emp.EmailId);
-            Assert.Equal(21, emp.Age);
-
-
+            var response = _controller.Post(emp);
+            response.ToString().Equals(HttpStatusCode.OK);
         }
     }
 }
